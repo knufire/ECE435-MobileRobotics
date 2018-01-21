7e0a765cd4e6b4db6f47dd845d6942c34c86948f
Rahul Yarlagadda <knufire@gmail.com>
2018-01-21 18:33:41 -0500

    Cleaned up drive code. Removes sonar code as it has been removed from the robot.

--- RobotDrive.h
+++ RobotDrive.h
@@ -1,42 +1,94 @@
-/*
+/**
  * RobotDrive.h
+ * 	Author: Rahul Yarlagadda, Ellie Honious
+ * 	Date: December 13th, 2017
  *
- *  Created on: Dec 20, 2017
- *      Author: Rahul
+ * 	This file contains the basic motor control and odometry code for the robot. Basic functions for driving forward, reverse
+ * 	and going to an angle are written.
  */
 
 #pragma once
 #ifndef ROBOTDRIVE_H_
 #define ROBOTDRIVE_H_
 
-#define robot_spd 	400 	//set robot speed
-#define max_accel 	5000	//maximum robot acceleration
-#define max_spd 	1000	//maximum robot speed
+/**
+ * These constants define kinematic traits of the robot, to be used by the AccelStepper library.
+ */
+#define robot_spd 	400 	//Desired robot speed in steps/s
+#define max_accel 	5000	//Maximum robot acceleration in steps/s^2
+#define max_spd 	1000	//Maximum robot speed in steps/s
 
-#define quarter_rotation 	200  	//stepper quarter rotation
-#define half_rotation 		400     //stepper half rotation
-#define one_rotation  		800     //stepper motor runs in 1/4 steps so 800 steps is one full rotation
-#define two_rotation  		1600    //stepper motor 2 rotations
-#define three_rotation 		2400   	//stepper rotation 3 rotations
-#define four_rotation 		3200    //stepper rotation 3 rotations
-#define five_rotation 		4000    //stepper rotation 3 rotations
+/**
+ * These constants are the number of motor steps for the specified number of rotations.
+ */
+#define quarter_rotation 	200
+#define half_rotation 		400
+#define one_rotation  		800
+#define two_rotation  		1600
+#define three_rotation 		2400
+#define four_rotation 		3200
+#define five_rotation 		4000
 
+
+/**
+ * These are experimentally defined constants to convert motor steps to real-world units.
+ */
 #define CONST_FEET_TO_STEPS 912.2
 #define CONST_SPIN_DEGREES_TO_STEPS 5.5
 
+/**
+ * These variables contain the current (x,y) position of the robot, along with the angle it is facing
+ * relative to startup. They are exernal, so the main program file can accces them.
+ */
 extern float robotX;
 extern float robotY;
 extern float robotAngle;
 
+/**
+ * Contains the initalizaiton routines for the stepper motor drivers and the AccelStepper library.
+ * This should be called in the init() function in the main program file.
+ */
 void driveSetup();
-void forward();
-void pivot();
-void spin();
+
+/**
+ * Moves the robot forward by the specified number of steps.
+ */
+void forward(int rot);
+
+/**
+ * Pivots the robot by moving one wheel for the specified number of steps.
+ * Pivots left if dir = 0, pivots right otherwise.
+ */
+void pivot(int rot, int dir);
+
+/**
+ * Spins the robot using both wheels by the specified number of steps.
+ * Positive number of steps spins the robot clockwise.
+ */
+void spin(int rot);
+/**
+ * Spins the robot by the specified number of degrees. Utilizes spin().
+ */
 void spinDegrees(float degrees);
+
+/**
+ * Stops both stepper motors.
+ */
 void stop();
-void reverse();
-void goToAngle();
+
+/**
+ * Moves the robot backwards by the specified number of steps.
+ */
+void reverse(int rot);
+
+/**
+ * Moves the robot to a specified angle. The robot's angle is 0 during startup.
+ */
+void goToAngle(float degrees);
+
+/**
+ * Blocks program execution until stepper movements have been completed.
+ */
 void runToStop();
-void runAtSpeed(float leftSpeed, float rightSpeed);
 
 #endif /* ROBOTDRIVE_H_ */
--- RobotDrive.ino
+++ RobotDrive.ino
@@ -1,10 +1,10 @@
-/*	RobotDrive.ino
+/**
+ * RobotDrive.ino
  * 	Author: Rahul Yarlagadda, Ellie Honious
  * 	Date: December 13th, 2017
  *
  * 	This file contains the basic motor control and odometry code for the robot. Basic functions for driving forward, reverse
- * 	and going to an angle are written. Additionally, the robot uses encoders to keep track of it's position and poise relative
- * 	to it's starting location.
+ * 	and going to an angle are written.
  */
 
 #include <AccelStepper.h>//include the stepper motor library
@@ -16,69 +16,68 @@ float robotX = 0;
 float robotY = 0;
 float robotAngle = 0;
 
-AccelStepper stepperRight(AccelStepper::DRIVER, PIN_RT_STEP, PIN_RT_DIR);//create instance of right stepper motor object (2 driver pins, low to high transition step pin 52, direction input pin 53 (high means forward)
-AccelStepper stepperLeft(AccelStepper::DRIVER, PIN_LT_STEP, PIN_LT_DIR);//create instance of left stepper motor object (2 driver pins, step pin 50, direction input pin 51)
-MultiStepper steppers;//create instance to control multiple steppers at the same time
+AccelStepper stepperRight(AccelStepper::DRIVER, PIN_RT_STEP, PIN_RT_DIR); //create instance of right stepper motor object (2 driver pins, low to high transition step pin 52, direction input pin 53 (high means forward)
+AccelStepper stepperLeft(AccelStepper::DRIVER, PIN_LT_STEP, PIN_LT_DIR); //create instance of left stepper motor object (2 driver pins, step pin 50, direction input pin 51)
+MultiStepper steppers; //create instance to control multiple steppers at the same time
 
 void driveSetup() {
-	//stepper Motor set up
 	Serial.println("Setting up drive");
 
-	pinMode(PIN_RT_STEP, OUTPUT);//sets pin as outputr
-	pinMode(PIN_RT_DIR, OUTPUT);//sets pin as output
-	pinMode(PIN_LT_STEP, OUTPUT);//sets pin as output
-	pinMode(PIN_LT_DIR, OUTPUT);//sets pin as output
-	pinMode(PIN_STEP_ENABLE, OUTPUT);//sets pin as output
+	pinMode(PIN_RT_STEP, OUTPUT); //sets pin as outputr
+	pinMode(PIN_RT_DIR, OUTPUT); //sets pin as output
+	pinMode(PIN_LT_STEP, OUTPUT); //sets pin as output
+	pinMode(PIN_LT_DIR, OUTPUT); //sets pin as output
+	pinMode(PIN_STEP_ENABLE, OUTPUT); //sets pin as output
 
-	digitalWrite(PIN_STEP_ENABLE, false);//turns on the stepper motor drivers
+	digitalWrite(PIN_STEP_ENABLE, false); 	//turns on the stepper motor drivers
 
-	stepperRight.setMaxSpeed(max_spd);//set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
-	stepperRight.setAcceleration(max_accel);//set desired acceleration in steps/s^2
-	stepperLeft.setMaxSpeed(max_spd);//set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
-	stepperLeft.setAcceleration(max_accel);//set desired acceleration in steps/s^2
+	stepperRight.setMaxSpeed(max_spd); //set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
+	stepperRight.setAcceleration(max_accel); //set desired acceleration in steps/s^2
+	stepperLeft.setMaxSpeed(max_spd); //set the maximum permitted speed limited by processor and clock speed, no greater than 4000 steps/sec on Arduino
+	stepperLeft.setAcceleration(max_accel); //set desired acceleration in steps/s^2
 
-	stepperRight.setSpeed(robot_spd);//set right motor speed
-	stepperLeft.setSpeed(robot_spd);//set left motor speed
+	stepperRight.setSpeed(robot_spd); //set right motor speed
+	stepperLeft.setSpeed(robot_spd); //set left motor speed
 
-	steppers.addStepper(stepperRight);//add right motor to MultiStepper
-	steppers.addStepper(stepperLeft);//add left motor to MultiStepper
-
+	steppers.addStepper(stepperRight); //add right motor to MultiStepper
+	steppers.addStepper(stepperLeft); //add left motor to MultiStepper
 }
 
 void forward(int rot) {
-	stepperRight.move(rot);  //move right motor to position
-	stepperLeft.move(rot);   //move left motor to position
-	runToStop();//run until the robot reaches the target
-//	float dx = cos(robotAngle * PI / 180) * rot / CONST_FEET_TO_STEPS;
-//	float dy = sin(robotAngle * PI / 180) * rot / CONST_FEET_TO_STEPS;
-//	robotX = robotX + dx;
-//	robotY = robotY + dy;
+	stepperRight.move(rot);	//move right motor to position
+	stepperLeft.move(rot); 	//move left motor to position
+	runToStop();   			//run until the robot reaches the target
+
+	//Update the robot's position
+	robotX += cos(robotAngle * PI / 180) * rot / CONST_FEET_TO_STEPS;
+	robotY += sin(robotAngle * PI / 180) * rot / CONST_FEET_TO_STEPS;
 }
 
-/*robot pivot function */
 void pivot(int rot, int dir) {
-  if (dir > 0) {//pivot right
-	  stepperLeft.move(rot);
-  }
-  else//pivot left
-  {
-    stepperRight.move(rot);
-  }
-  runToStop();                       //run until the robot reaches the target
+	if (dir > 0) {
+		stepperLeft.move(rot);
+	} else {
+		stepperRight.move(rot);
+	}
+	runToStop();
+	//TODO: Update robot postition variables.
 }
 
-/*robot spin function */
-void spin(int rot, int dir) {
-  if (dir > 0) {//spin right
-	 stepperRight.move(-rot);
-     stepperLeft.move(rot);
-  }
-  else//spin left
-  {
-	 stepperRight.move(-rot);
-	 stepperLeft.move(rot);
-  }
-  runToStop();                        //run until the robot reaches the target
+void spin(int rot) {
+	stepperRight.move(-rot);
+	stepperLeft.move(rot);
+	runToStop();
+
+	//Update the robot's angle.
+	robotAngle += rot / CONST_SPIN_DEGREES_TO_STEPS;
+
+	//Ensure that the robot angle is between 0 and 360 degrees.
+	while (robotAngle < 0) {
+		robotAngle += 360;
+	}
+	while (robotAngle > 360) {
+		robotAngle -= 360;
+	}
 }
 
 void stop() {
@@ -86,7 +85,6 @@ void stop() {
 	stepperLeft.stop();
 }
 
-/*robot move reverse function */
 void reverse(int rot) {
 	forward(-rot);
 }
@@ -96,37 +94,31 @@ void spinDegrees(float degrees) {
 }
 
 void goToAngle(float degrees) {
+	//Use the robot's current angle to figure out the needed change in angle
 	float dAngle = degrees - robotAngle;
-	while(dAngle > 180){
+
+	//Ensure the change in angle is between -180 and 180 for the shortest spin possible
+	while (dAngle > 180) {
 		dAngle = dAngle - 360;
 	}
-	while(dAngle < -180){
+	while (dAngle < -180) {
 		dAngle = dAngle + 360;
 	}
+
+	//Convert change in angle into motor steps
 	long numSteps = dAngle * CONST_SPIN_DEGREES_TO_STEPS;
-	stepperRight.move(numSteps);
-	stepperLeft.move(numSteps * -1);
-	runToStop();
-	//TODO: Reimplement navigation
-//	while (degrees < 0) {
-//		degrees += 360;
-//	}
-//	while (degrees > 360) {
-//		degrees -= 360;
-//	}
-//	robotAngle = degrees;
+	spin(numSteps);
+
+	//Update new robot angle
+	while (degrees < 0) {
+		degrees += 360;
+	}
+	while (degrees > 360) {
+		degrees -= 360;
+	}
+	robotAngle = degrees;
 }
 
-/*This function, runToStop(), will run the robot until the target is achieved and
-   then stop it
- */
-void runToStop ( void ) {
+void runToStop(void) {
 	steppers.runSpeedToPosition();
-}
-
-void runAtSpeed(float leftSpeed, float rightSpeed) {
-	stepperLeft.setSpeed(leftSpeed);
-	stepperRight.setSpeed(rightSpeed);
-	stepperLeft.runSpeed();
-	stepperRight.runSpeed();
 }
